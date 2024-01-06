261e7298aac50163e7cd5cc2ae5086f257156cf9
zelkovaria <hyein396@gmail.com>
2024-01-06 23:21:59 +0900

    14.3 newsapi API 키 발급받기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=b436137ca61e4accb95db491e388fcf5"
       );
       setData(response.data);
     } catch (e) {
