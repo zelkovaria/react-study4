b5f46af576572b7230d8bdb8ce543c388a3ded07
zelkovaria <hyein396@gmail.com>
2024-01-07 18:08:46 +0900

    14.7.2 NewsPage 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { useState, useCallback } from "react";
-import Categories from "./components/Categories";
-import NewsList from "./components/NewsList";
+import { Route, Routes } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-  const [category, setCategory] = useState("all");
-  const onSelect = useCallback((category) => setCategory(category), []);
-
   return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 
